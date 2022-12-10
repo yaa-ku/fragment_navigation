626c42681bb36e9643923fb9a51cda3709b562e3
yanakuz <yana.yana.kuznecova666@gmail.com>
2022-12-11 00:54:34 +1000

    added navigation

--- app/src/main/java/com/example/fragment_navigation/MainActivity.kt
+++ app/src/main/java/com/example/fragment_navigation/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.fragment_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragment_navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                    R.id.home -> {
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frame_layout, HomeFragment())
+                        transaction.commit()
+                    }
+                    R.id.weather -> {
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frame_layout, WeatherFragment())
+                        transaction.commit()
+                    }
+                    R.id.charts -> {
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frame_layout, ChartsFragment())
+                        transaction.commit()
+                    }
+                    R.id.profile -> {
+                        val transaction = supportFragmentManager.beginTransaction()
+                        transaction.replace(R.id.frame_layout, ProfileFragment())
+                        transaction.commit()
+                    }
+             }
+            true
+        }
     }
 }
