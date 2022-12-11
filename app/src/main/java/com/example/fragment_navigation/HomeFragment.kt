d67a723e9b53a2b28d1b693f33aa9fe6c8ab34eb
yanakuz <yana.yana.kuznecova666@gmail.com>
2022-12-11 13:01:09 +1000

    added buttons

--- app/src/main/java/com/example/fragment_navigation/HomeFragment.kt
+++ app/src/main/java/com/example/fragment_navigation/HomeFragment.kt
@@ -35,6 +35,7 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+        
     }
 
     companion object {
