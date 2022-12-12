316443d1d962fcb89b4142731d9adb7fe88c3a67
yanakuz <yana.yana.kuznecova666@gmail.com>
2022-12-12 18:20:54 +1000

    added weather

--- app/src/main/java/com/example/fragment_navigation/WeatherFragment.kt
+++ app/src/main/java/com/example/fragment_navigation/WeatherFragment.kt
@@ -1,59 +1,102 @@
 package com.example.fragment_navigation
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.fragment_navigation.databinding.FragmentWeatherBinding
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WeatherFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+const val API_KEY = "70e3202ca944469089552814220912"
 class WeatherFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentWeatherBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false)
+    ): View {
+        binding = FragmentWeatherBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WeatherFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WeatherFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.getWeather.setOnClickListener {
+            var city = binding.searchCity.text.toString()
+            getResult(city)
+        }
+    }
+
+    private fun getResult(city: String){
+        val url = "https://api.weatherapi.com/v1/forecast.json" +
+                "?key=$API_KEY&q=$city&days=10&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                    result-> parseWeatherData(result)
+            },
+            {
+                    error -> Log.d("MyLog","Error: $error")
             }
+        )
+        queue.add(request)
+    }
+    private fun parseWeatherData(result: String) {
+        val mainObject = JSONObject(result)
+        val daysArray = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+        val day = daysArray[0] as JSONObject
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            day.getJSONObject("day").getString("maxtemp_c"),
+            day.getJSONObject("day").getString("mintemp_c"),
+            mainObject.getJSONObject("current").getString("wind_mph"),
+            day.getJSONObject("astro").getString("sunrise"),
+            day.getJSONObject("astro").getString("sunset"),
+            mainObject.getJSONObject("current").getString("pressure_mb"),
+            mainObject.getJSONObject("current").getString("humidity"),
+            mainObject.getJSONObject("current").getString("cloud")
+        )
+        binding.sunrise.text = item.sunrise
+        binding.sunset.text = item.sunset
+        binding.city.text = item.city
+        binding.temp.text = item.currentTemp + "°C"
+        binding.tempMax.text = item.maxTemp
+        binding.tempMin.text = item.minTemp
+        binding.humidity.text = item.humidity
+        binding.pressure.text = item.pressure
+        binding.wind.text = item.wind
+        binding.status.text = item.condition
+        binding.cloud.text = item.cloud
+        binding.timeDate.text = item.time
+
+        /*Log.d("MyLog", "City: ${item.city}")
+        Log.d("MyLog", "Time: ${item.time}")
+        Log.d("MyLog", "Condition: ${item.condition}")
+        Log.d("MyLog", "Temp: ${item.currentTemp}")
+        Log.d("MyLog", "Wind: ${item.wind}")
+        Log.d("MyLog", "Max: ${item.maxTemp}")
+        Log.d("MyLog", "Min: ${item.minTemp}")
+        Log.d("MyLog", "SunRise: ${item.sunrise}")
+        Log.d("MyLog", "SunSet: ${item.sunset}")
+        Log.d("MyLog", "pressure_mb: ${item.pressure}")
+        Log.d("MyLog", "humidity: ${item.humidity}")*/
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = WeatherFragment()
     }
 }
---
+++ app/src/main/java/com/example/fragment_navigation/WeatherModel.kt
@@ -0,0 +1,18 @@
+package com.example.fragment_navigation
+
+data class WeatherModel(
+    val city: String,
+    val time: String,
+    val condition: String,
+    val currentTemp: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val wind: String,
+    val sunrise: String,
+    val sunset: String,
+    val pressure: String,
+    val humidity: String,
+    val cloud: String
+)
+
+
